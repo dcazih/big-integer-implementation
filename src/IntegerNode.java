c7210a04e085d2a21c52ebd749d3c5ff99590d8d
Dmitri Azih <dmitriazih@gmail.com>
2024-03-20 18:27:42 -0600

    added class skeleton

---
+++ src/IntegerNode.java
@@ -0,0 +1,6 @@
+public class IntegerNode {
+
+    int digits; // 4 digits in the current node
+    IntegerNode higher_positions; // digits in the higher positions
+
+}
---
+++ src/MyBigInteger.java
@@ -0,0 +1,11 @@
+public class MyBigInteger {
+
+    public MyBigInteger(){
+
+    }
+
+    public static MyBigInteger add(MyBigInteger int1, MyBigInteger int2){
+        return null;
+    }
+
+}
