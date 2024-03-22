4fee8e9aee66a053cb707715f14683851e8ea1bb
Dmitri Azih <dmitriazih@gmail.com>
2024-03-22 15:08:05 -0600

    moved IntegerNode class inside MyBigInteger class

--- src/MyBigInteger.java
+++ src/MyBigInteger.java
@@ -1,27 +1,33 @@
 public class MyBigInteger {
+    class IntegerNode {
 
+        int digits; // 4 digits in the current node
+        IntegerNode higher_positions; // digits in the higher
+
+        public IntegerNode(int digits){
+            this.digits = digits;
+            higher_positions = null;
+        }
+
+    }
     IntegerNode sign;
 
     public MyBigInteger(String num){
 
         // Create first node
         sign = new IntegerNode(0);
-        
+
+        // Parse num by 4 chars, convert to int, add node
+        if (num.charAt(0) == '-'){}
+        else{
+            for(int i = 0; i < ( (num.length()%4==0) ? (num.length()/4) : ((num.length()/4) +1) ); i+=4){
+
+            }
+        }
     }
 
     public static MyBigInteger add(MyBigInteger int1, MyBigInteger int2){
         return null;
-    }
-
-}
-class IntegerNode {
-
-    int digits; // 4 digits in the current node
-    IntegerNode higher_positions; // digits in the higher
-
-    public IntegerNode(int digits){
-        this.digits = digits;
-        higher_positions = null;
     }
 
 }
