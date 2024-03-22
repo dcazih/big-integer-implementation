404bd77a675f866f2d1675f9e057919710c66ebf
Dmitri Azih <dmitriazih@gmail.com>
2024-03-22 11:19:28 -0600

    moved IntegerNode inside MyBigInteger, added constructor

--- src/MyBigInteger.java
+++ src/MyBigInteger.java
@@ -1,11 +1,27 @@
 public class MyBigInteger {
 
-    public MyBigInteger(){
+    IntegerNode sign;
 
+    public MyBigInteger(String num){
+
+        // Create first node
+        sign = new IntegerNode(0);
+        
     }
 
     public static MyBigInteger add(MyBigInteger int1, MyBigInteger int2){
         return null;
+    }
+
+}
+class IntegerNode {
+
+    int digits; // 4 digits in the current node
+    IntegerNode higher_positions; // digits in the higher
+
+    public IntegerNode(int digits){
+        this.digits = digits;
+        higher_positions = null;
     }
 
 }
