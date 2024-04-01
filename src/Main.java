7893fd655fabdf63c5a20f47dd09c8c2d60d645d
Dmitri Azih <dmitriazih@gmail.com>
2024-04-01 15:59:55 -0600

    updated main for testing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@ public class Main {
 
     public static void main(String[] args){
 
+
+        MyBigInteger num1 = new MyBigInteger("987654321");
+        MyBigInteger num2 = new MyBigInteger("123456789" );
+
+        MyBigInteger sum = MyBigInteger.add(num1, num2);
+
+        System.out.printf("%s\n%s +\n--------------\n%s\n", num1, num2, sum);
+
+
+        //System.out.println(MyBigInteger.add(sum, num2));
+        //System.out.println(num1.sign.higher_positions.digits);
+
+        //System.out.printf("Is %s greater than %s: %s ", num1, num2, num1.greaterThanOrEqual(num2));
+        //System.out.println(MyBigInteger.add(num1, num2));
     }
 
 }
